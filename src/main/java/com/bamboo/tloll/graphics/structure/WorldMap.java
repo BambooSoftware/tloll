8d8a21df1e8b478f754ec66017f37bae5fd44617
Andrew Blackburn <andrew.blackburn@protonmail.com>
2016-12-09 18:26:28 -0500

    testing with world map

--- src/main/java/com/bamboo/tloll/graphics/structure/WorldMap.java
+++ src/main/java/com/bamboo/tloll/graphics/structure/WorldMap.java
@@ -1,13 +1,38 @@
 package com.bamboo.tloll.graphics.structure;
 
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.experimental.dag.DirectedAcyclicGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedMultigraph;
+import org.jgrapht.graph.SimpleDirectedGraph;
 
 public class WorldMap {
 
 
-    //This needs to be a graph structure.
+    DirectedGraph<MapPiece, DefaultEdge> worldMap;
+ad
 
-    //DefaultDirectedGraph<> worldMap =
+    public void WorldMap() {
 
+        worldMap = new DirectedMultigraph<>();
+
+        MapPiece mp1 = new MapPiece();
+        MapPiece mp2 = new MapPiece();
+        MapPiece mp3 = new MapPiece();
+        MapPiece mp4 = new MapPiece();
+
+
+        worldMap.addVertex(mp1);
+        worldMap.addVertex(mp2);
+        worldMap.addVertex(mp3);
+        worldMap.addVertex(mp4);
+
+        worldMap.addEdge(mp1, mp2);
+
+        //https://github.com/jgrapht/jgrapht/wiki/HelloWorld
+
+    }
 
 
 }
