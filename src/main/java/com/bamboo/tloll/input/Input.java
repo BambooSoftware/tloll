ff7b3aef1d1ac48baf232393f1222f1441e81849
michaelplatt07 <michael.platt.07@gmail.com>
2017-03-19 11:23:48 -0400

    MAP : Updated the input to do a cheesy diagonal movement.

--- src/main/java/com/bamboo/tloll/input/Input.java
+++ src/main/java/com/bamboo/tloll/input/Input.java
@@ -5,17 +5,23 @@ import com.bamboo.tloll.graphics.Direction;
 import com.bamboo.tloll.graphics.Unit;
 import com.bamboo.tloll.physics.PhysicsEngine;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import static org.lwjgl.glfw.GLFW.*;
 
 public class Input {
 
-    //TODO: need to fix vector math - diag movement is really really fast
+    // TODO(map) : We have a cheesed out diagonal.  Should do proper vector math instead.
+    // TODO(map) : The player stands still on diagonal against a wall.  Should slide instead.
     //TODO: Also direct movement may be better served inside the player. tbd.
 
     /*
      * The slick callback wont wory because its too laggy with the difference between key press and key repeat.
      * Functionally useless.
      */
+    List<Character> inputList = new ArrayList<>();
+
     public void pollInput(long windowId, Unit player) {
         handleKeyPresses(windowId, player);
     }
@@ -30,41 +36,58 @@ public class Input {
 
     private void handleKeyW(Unit player) {
         if(KeyboardHandler.isKeyDown(GLFW_KEY_W)) {
-            keyPressW(player);
+	    if (inputList.indexOf('W') == -1)
+		inputList.add('W');
+	    keyPressW(player);
         } else {
-            keyReleaseW(player);
+	    keyReleaseW(player);
+	    if (inputList.indexOf('W') != -1)
+		inputList.remove(inputList.indexOf('W'));
         }
     }
 
     private void handleKeyS(Unit player) {
         if(KeyboardHandler.isKeyDown(GLFW_KEY_S)) {
+	    if (inputList.indexOf('S') == -1)
+		inputList.add('S');
             keyPressS(player);
         } else {
             keyReleaseS(player);
+	    if (inputList.indexOf('S') != -1)
+		inputList.remove(inputList.indexOf('S'));
         }
     }
 
     private void handleKeyA(Unit player) {
         if(KeyboardHandler.isKeyDown(GLFW_KEY_A)) {
-            keyPressA(player);
+	    if (inputList.indexOf('A') == -1)
+		inputList.add('A');
+	    keyPressA(player);
         } else {
             keyReleaseA(player);
+	    if (inputList.indexOf('A') != -1)
+		inputList.remove(inputList.indexOf('A'));
         }
     }
 
     private void handleKeyD(Unit player) {
         if(KeyboardHandler.isKeyDown(GLFW_KEY_D)) {
+	    if (inputList.indexOf('D') == -1)
+		inputList.add('D');
             keyPressD(player);
         } else {
             keyReleaseD(player);
+	    if (inputList.indexOf('D') != -1)
+		inputList.remove(inputList.indexOf('D'));
         }
     }
 
 
     private void keyPressW(Unit player) {
+	System.out.println("Speed Y : " + (Constants.MAX_SPEED_Y * (1.0F / inputList.size())));
         player.setSpeedY(player.getSpeedY() + player.getAcceleration());
-        if (player.getSpeedY() > Constants.MAX_SPEED_Y) {
-            player.setSpeedY(Constants.MAX_SPEED_Y);
+        if (player.getSpeedY() > (Constants.MAX_SPEED_Y * (1.0F / inputList.size()))) {
+	    player.setSpeedY(Constants.MAX_SPEED_Y * (1.0F / inputList.size()));
         }
         PhysicsEngine.movePlayer(player, player.getSpeedX(), player.getSpeedY());
         player.setDirection(Direction.UP);
@@ -89,8 +112,8 @@ public class Input {
     private void keyPressS(Unit player) {
 
         player.setSpeedY(player.getSpeedY() - player.getAcceleration());
-        if (player.getSpeedY() < Constants.MIN_SPEED_Y) {
-            player.setSpeedY(Constants.MIN_SPEED_Y);
+        if (player.getSpeedY() < (Constants.MIN_SPEED_Y * (1.0F / inputList.size()))) {
+            player.setSpeedY(Constants.MIN_SPEED_Y * (1.0F / inputList.size()));
         }
         PhysicsEngine.movePlayer(player, player.getSpeedX(), player.getSpeedY());
         player.setDirection(Direction.DOWN);
@@ -115,8 +138,8 @@ public class Input {
 
     private void keyPressA(Unit player) {
         player.setSpeedX(player.getSpeedX() - player.getAcceleration());
-        if (player.getSpeedX() < Constants.MIN_SPEED_X) {
-            player.setSpeedX(Constants.MIN_SPEED_X);
+        if (player.getSpeedX() < (Constants.MIN_SPEED_X * (1.0F / inputList.size()))) {
+            player.setSpeedX(Constants.MIN_SPEED_X * (1.0F / inputList.size()));
         }
         PhysicsEngine.movePlayer(player, player.getSpeedX(), player.getSpeedY());
         player.setDirection(Direction.LEFT);
@@ -143,8 +166,8 @@ public class Input {
 
     private void keyPressD(Unit player) {
         player.setSpeedX(player.getSpeedX() + player.getAcceleration());
-        if (player.getSpeedX() > Constants.MAX_SPEED_X) {
-            player.setSpeedX(Constants.MAX_SPEED_X);
+        if (player.getSpeedX() > (Constants.MAX_SPEED_X * (1.0F / inputList.size()))) {
+            player.setSpeedX(Constants.MAX_SPEED_X * (1.0F / inputList.size()));
         }
         PhysicsEngine.movePlayer(player, player.getSpeedX(), player.getSpeedY());
         player.setDirection(Direction.RIGHT);
